368ce7bda17ac8490600c2b1ecbcf145789f01f6
Kiwavi <dekamotho@gmail.com>
2024-10-12 19:15:30 +0300

    feat main: add endpoint, install express-validator

--- server.js
+++ server.js
@@ -1,9 +1,38 @@
 const express = require("express");
 const app = express();
-const port = 3000;
+const port = 8007;
+const prisma = require("./db/prisma");
+app.use(express.json());
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
+});
+
+app.post("/create-user", async (req, res) => {
+  try {
+    let {name, email, password} = req.body;
+    if (!name || !email || !password) {
+      return res
+        .status(400)
+        .json({success: false, message: "Missing some required input"});
+    }
+
+    if (name && email && password) {
+      await prisma.User.create({
+        data: {
+          name,
+          email,
+          password,
+        },
+      });
+      return res.status(200).json({success: true});
+    }
+  } catch (e) {
+    console.log(e);
+    return res
+      .status(500)
+      .json({success: false, message: "Internal server error"});
+  }
 });
 
 app.listen(port, () => {
